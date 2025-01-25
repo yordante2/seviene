aa840685ae91f9fd8a35a9f2b9e30c820fcf857c
yordante <80213792+yordante2@users.noreply.github.com>
2025-01-25 13:00:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ API_HASH = "6cad012cd66ba62e1b7d49f1f84742d1"
 BOT_TOKEN = "7279915670:AAHOPZhjXd-KBq9bUICQj-jdqI-edi0XXYQ"
 
 bot = Client("moodle", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-data = {"moodle": "", "token": ""}
+data = {"moodle": "", "token": "", "ws": True}
 
 @bot.on_message()
 async def messages_handler(client: Client, message: Message):
@@ -30,12 +30,18 @@ async def messages_handler(client: Client, message: Message):
 			print(data)
 		except Exception as ex:
 			await message.reply(ex)
+	elif msg.startswith("/ws"):
+		if msg.split(" ")[1] == "on":
+			data["ws"] = True
+		elif msg.split(" ")[1] == "off":
+			data["ws"] = False
+		await message.reply("WebService: " + str(data["ws"]))
 	elif msg.startswith("http"):
 		try:
 			print(data)
 			#await message.reply("Procesando...")
 			file = extras.download_file(msg)
-			link = moodleclient.upload_token(file, data["token"], data["moodle"])
+			link = moodleclient.upload_token(file, data["token"], data["moodle"], data["ws"])
 			await message.reply(link)
 		except Exception as ex:
 			await message.reply(ex)
